f3f57c979057d2487463f110b2fc54598df5dc29
Claudia Donea <c.donea@resilientplc.com>
2016-11-04 17:01:25 +0000

    file structure + paths config + file loader

---
+++ configs/paths.config.js
@@ -0,0 +1,20 @@
+
+var path = require('path');
+
+var publicPaths = (function(){
+    var base = './../';
+    var app = base + 'app/';
+    var babelSettings = {
+      extends: path.join(__dirname, '/.babelrc')
+    };
+
+    return {
+      base: base,
+      app: app,
+      entry: app + 'main/js/index.js',
+      dist: base + '/dist',
+      babelPath: 'babel?' + JSON.stringify(babelSettings)
+    };
+})();
+
+module.exports = publicPaths;
--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -1,25 +1,27 @@
 var webpack = require('webpack');
 var path = require('path');
+var pathsConfig = require('./paths.config');
+
+console.log("==================");
+
 var config = {
     entry: [
     'webpack-dev-server/client?http://localhost:8080/', // WebpackDevServer host and port
     'webpack/hot/dev-server', // "only" prevents reload on syntax errors
-    './index.js' // Your appʼs entry point
+    pathsConfig.entry
   ],
   output: {
     filename: 'main.js',
     publicPath: '/',
-    path: './dist'
+    path: pathsConfig.dist
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
+        include: [ path.join(__dirname, pathsConfig.app)],
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel'//,
-        // query: {
-        //   presets: ['es2015','react']
-        // }
+        loader: pathsConfig.babelPath//'babel'
       }
     ]
   },
@@ -36,7 +38,7 @@ var config = {
   },
   devServer: {
     hot: true,
-    contentBase: './'
+    contentBase: pathsConfig.base
   }
 };
 
